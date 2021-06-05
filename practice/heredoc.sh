c539fd6debc8340b3c00546fadde4f014efacc29
purni <purnima.annangi@gmail.com>
2021-06-05 00:18:48 -0500

    heredoc

--- practice/heredoc.sh
+++ practice/heredoc.sh
@@ -3,3 +3,13 @@ cat << EOF
 The user is : $USER
 The home for $USER is : $HOME
 EOF
+
+cat << Aahna > heredoc.txt
+This is $USER
+The home is : $HOME
+Aahna
+
+cat << Aarush | grep home
+This is $USER
+The home is : $HOME
+Aarush
