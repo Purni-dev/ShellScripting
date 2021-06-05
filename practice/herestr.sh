2fea2cced6b5fba5304924f0d69ff377b2bc77f9
purni <purnima.annangi@gmail.com>
2021-06-04 19:04:46 -0500

    herestring

--- practice/herestr.sh
+++ practice/herestr.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-echo "This is Sai Ram" | tr [:lower:] [:upper:]
-
+echo "Example of echo Converting string from lower to uppercase :"
+echo "This is Sai Ram" | tr [:lower:] [:upper:]   
 echo "==============================="
-
+echo "Example of Here String Converting string from upper to lowercase : "
 tr [:upper:] [:lower:] <<< "This is Sai Ram"
-
+echo "============================"
+echo "Example of Here String  Converting string from upper to lowercase "
+name="BASH SHELL SCRIPTING"
+tr [A-Z] [a-z] <<< $name
+echo "============================"
+echo "Need to display docker version in UPPERCASE letters"
+tr [a-z] [A-Z] <<<$(docker -v)
+docker -v | tr [a-z] [A-Z]
