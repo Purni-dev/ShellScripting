4e7f6fef910c12c3986c30c5d1c088b059535a9f
purni <purnima.annangi@gmail.com>
2021-06-04 19:36:21 -0500

    comments

---
+++ practice/comment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+<< MYCOM
+This is about comments.
+There are 2 types
+Single line & Multi line comments
+MYCOM
+
+#Single line is used with # and multi line as example above and below
+
+: '
+This is example of multi line comment to.
+We can use 1st one or this one for multi line comments
+'
+
+echo "This is about single line and multi line comments"
