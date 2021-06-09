e242baa816ac341f25dfb4e7a43b00b854b7d20c
purni <purnima.annangi@gmail.com>
2021-06-09 13:45:57 -0500

    Docker-Start

--- practice/docker_start.sh
+++ practice/docker_start.sh
@@ -10,11 +10,30 @@ else
 fi
 dockerstart
 
+if [[ $(id -u) -eq 0 ]]
+then
+
 if systemctl status docker 1>/dev/null 2>/dev/null
+then
+     echo "Already docker is up and running"
+else 
+     echo "Starting docker service"
+     systemctl start docker
+     echo "Successfully started docker service"
+fi
+else
+
+if sudo -v  1>/dev/null 2>/dev/null 
+then
+   if systemctl status docker 1>/dev/null 2>/dev/null
 then
    echo "Already docker is up and running"
 else
    echo "Starting docker service"
-   systemctl start docker
+   sudo systemctl start docker
    echo "Successfully started docker service"
+fi  
+else
+   echo "You are not allowed to start docker service beause you are not root user or you dont have sudo privileges"
+fi
 fi
