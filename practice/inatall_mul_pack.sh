4a24b694c7dedde4f715a7ccd2f40e20cbbc7614
purni <purnima.annangi@gmail.com>
2021-06-11 13:16:35 -0500

    Multiple-Pakgs-Forloop

---
+++ practice/inatall_mul_pack.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear
+if [[ $(id -u) -ne 0 ]]
+then
+   echo "You should be root user to install packages or you can install with sudo privileges"
+   exit 1
+fi
+
+for each_pkg in vim nginx httpd
+do
+if which $each_pkg &> /dev/null
+then
+    echo "Already $each_pkg package installed"
+else
+    echo "Installing package $each_pkg"
+    yum install $each_pkg  -y  &> /dev/null
+    if [[ $? -eq 0 ]]
+    then
+      echo "Successfully installed $each_pkg"
+    else
+      echo "Unable to install $each_pkg"
+    fi
+
